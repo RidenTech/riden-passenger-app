9304ff4eefc8fccbad8d83085abd83a595bc459c
RidenTech <Ridentech@icloud.com>
2026-04-21 03:33:16 -0700

    Delete truncate.py

--- truncate.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-with open('lib/my_profile/ProfileSettingBottomSheet.dart', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-with open('lib/my_profile/ProfileSettingBottomSheet.dart', 'w', encoding='utf-8') as f:
-    f.writelines(lines[:416])
