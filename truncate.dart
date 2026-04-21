aa5c9b6d358d563d1fa7779ee937ddb067d78369
RidenTech <Ridentech@icloud.com>
2026-04-21 03:33:07 -0700

    Delete truncate.dart

--- truncate.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-
-void main() {
-  final file = File('lib/my_profile/ProfileSettingBottomSheet.dart');
-  final lines = file.readAsLinesSync();
-  file.writeAsStringSync(lines.take(416).join('\n') + '\n');
-}
